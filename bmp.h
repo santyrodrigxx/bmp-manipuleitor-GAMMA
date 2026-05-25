7cc04cd4a74c4e29bcb5576ef03a641e4a9d190e
Anto <130939529+AntonellaGahonaR@users.noreply.github.com>
2026-05-25 18:55:58 -0300

    Bloque de BMP
    Bloque de lectura, validacion y gestion de datos del BMP.

---
+++ bmp.h
@@ -0,0 +1,81 @@
+#ifndef BMP_H_INCLUDED
+#define BMP_H_INCLUDED
+
+#include "funciones_grupo.h"
+
+/*
+    BMP FILE HEADER - 14 bytes
+        ------------------------------------------------
+        Byte 0-1     firma              2 bytes
+        Byte 2-5     tamanioArchivo     4 bytes
+        Byte 6-7     reservado1         2 bytes
+        Byte 8-9     reservado2         2 bytes
+        Byte 10-13   posicionPixeles    4 bytes
+
+
+    BMP INFO HEADER - 40 bytes
+        ------------------------------------------------
+        Byte 14-17   tamanioCabecera        4 bytes
+        Byte 18-21   anchoImagen            4 bytes
+        Byte 22-25   altoImagen             4 bytes
+        Byte 26-27   planos                 2 bytes
+        Byte 28-29   bitsPorPixel           2 bytes
+        Byte 30-33   compresion             4 bytes
+        Byte 34-37   tamanioImagen          4 bytes
+        Byte 38-41   resolucionHorizontal   4 bytes
+        Byte 42-45   resolucionVertical     4 bytes
+        Byte 46-49   coloresUsados          4 bytes
+        Byte 50-53   coloresImportantes     4 bytes
+*/
+
+typedef struct
+{
+    unsigned short firma;
+    unsigned int tamArchivo;
+    unsigned short reservado1;
+    unsigned short reservado2;
+    unsigned int posPixeles;
+} BMPArcCabecera;
+
+typedef struct
+{
+    unsigned int tamCabecera;
+    int anchoImagen;
+    int altoImagen;
+    unsigned short planos;
+    unsigned short bitsPorPixel;
+    unsigned int compresion;
+    unsigned int tamImagen;
+    int resolucionHorizontal;
+    int resolucionVertical;
+    unsigned int coloresUsados;
+    unsigned int coloresImportantes;
+} BMPInfoCabecera;
+
+typedef struct
+{
+    BMPArcCabecera archivo;
+    BMPInfoCabecera info;
+} BMPCabecera;
+
+typedef struct
+{
+    unsigned char azul;
+    unsigned char verde;
+    unsigned char rojo;
+} tPixelBMP;
+
+typedef struct
+{
+    BMPCabecera cabecera;
+    tda_matriz pixeles;
+} tImagenBMP;
+
+int bmp_leer_imagen(const char *nombreArchivo, tImagenBMP *imagen);
+int bmp_escribir_imagen(const char *nombreArchivo, const tImagenBMP *imagen);
+int bmp_validar_cabecera(const BMPCabecera *cabecera);
+void bmp_destruir_imagen(tImagenBMP *imagen);
+void bmp_imprimir_info(const tImagenBMP *imagen);
+
+
+#endif // BMP_H_INCLUDED

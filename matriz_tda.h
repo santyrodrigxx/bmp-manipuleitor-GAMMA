703a859425fd84fb8d17bbcbaea510dd63ce3265
Anto <130939529+AntonellaGahonaR@users.noreply.github.com>
2026-05-25 19:08:26 -0300

    Bloque matriz_TDA
    Se va a encargar de la matriz de pixeles, gestionando principalmente las operaciones que se harán en la matriz en general (la estructura pixeles estará declarada en bmp).

---
+++ matriz_tda.h
@@ -0,0 +1,46 @@
+#ifndef MATRIZ_TDA_H_INCLUDED
+#define MATRIZ_TDA_H_INCLUDED
+
+// VARIAS FUNCIONES DEBERIAN OPERAR CON TDA MATRIZ
+// se va a encargar de manipular los pixeles en mem
+
+typedef struct
+{
+    void **datos;
+    unsigned int filas;
+    unsigned int columnas;
+    size_t tamElemento;
+} tda_matriz;
+
+int matriz_crear(tda_matriz *matriz,
+                 unsigned int filas,
+                 unsigned int columnas,
+                 size_t tamElemento);
+
+void matriz_destruir(tda_matriz *matriz);
+
+int matriz_set(tda_matriz *matriz,
+               unsigned int fila,
+               unsigned int columna,
+               const void *elemento);
+
+int matriz_get(const tda_matriz *matriz,
+               unsigned int fila,
+               unsigned int columna,
+               void *elemento);
+
+void *matriz_obtener_ptr(tda_matriz *matriz,
+                         unsigned int fila,
+                         unsigned int columna);
+
+const void *matriz_obtener_ptr_const(const tda_matriz *matriz,
+                                     unsigned int fila,
+                                     unsigned int columna);
+
+unsigned int matriz_filas(const tda_matriz *matriz); //para que el procesador (grupo) trabaje con la matriz, no solo se ate a bmp
+
+unsigned int matriz_columnas(const tda_matriz *matriz);
+
+size_t matriz_tam_elemento(const tda_matriz *matriz);
+
+#endif // MATRIZ_TDA_H_INCLUDED

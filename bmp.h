4b1b6d9a009c3961585b37c454b54dd818e73eb6
Anto <130939529+AntonellaGahonaR@users.noreply.github.com>
2026-05-31 02:05:25 -0300

    Cambios prototipos de funcion // comentarios

--- bmp.h
+++ bmp.h
@@ -28,6 +28,8 @@
         Byte 50-53   coloresImportantes     4 bytes
 */
 
+#pragma pack(push, 1) //para que no haya padding entre los campos de las estructuras
+
 typedef struct
 {
     unsigned short firma;
@@ -58,6 +60,7 @@ typedef struct
     BMPInfoCabecera info;
 } BMPCabecera;
 
+
 typedef struct
 {
     unsigned char azul;
@@ -71,11 +74,33 @@ typedef struct
     tda_matriz pixeles;
 } tImagenBMP;
 
-int bmp_leer_imagen(const char *nombreArchivo, tImagenBMP *imagen);
+#pragma pack(pop) //restauro el packing por defecto
+
+int bmp_leer_imagen(const char *, tImagenBMP *);
+//llama a matriz crear para reservar la memoria de la matriz de pixeles, y luego lee los pixeles del archivo y los almacena en la matriz
+
+/*int bmp_escribir_pixel(const char *nombreArchivo, const tImagenBMP *imagen, unsigned int fila, unsigned int columna);
+aca fume y de la buena, no den bola a esta */
+
 int bmp_escribir_imagen(const char *nombreArchivo, const tImagenBMP *imagen);
+//llama a matriz obtener ptr para obtener la direccion de cada pixel en la matriz, 
+//y luego escribe los pixeles en el archivo, respetando el formato BMP (bottom-up, padding, etc)
+
 int bmp_validar_cabecera(const BMPCabecera *cabecera);
+/*tengo que ver el tema de las imagenes "bottom-up" (de abajo hacia arriba) o "top-down" (de arriba hacia abajo): 
+porque eso afecta el orden de lectura de los pixeles, y por lo tanto la validacion de la cabecera.
+Si es "top-down" el alto de la imagen es negativo, y si es "bottom-up" el alto de la imagen es positivo, 
+entonces tengo que validar que el alto de la imagen sea positivo para admitir solo imagenes "bottom-up", 
+o validar que el alto de la imagen sea negativo para admitir solo imagenes "top-down", 
+o validar que el alto de la imagen sea diferente de cero para admitir ambas tipos de imagenes*/
+
 void bmp_destruir_imagen(tImagenBMP *imagen);
-void bmp_imprimir_info(const tImagenBMP *imagen);
+//llama a matriz destruir para liberar la memoria de la matriz de pixeles, y luego libera la memoria del struct tImagenBMP
+//TENGO QUE VER COMO JOEL IMPLEMENTO LA FUNCION DE DESTRUIRMATRIZ, PARA PODER LIBERAR LA MEMORIA DEL STRUCT tImagenBMP
+
+void bmp_imprimir_info(const tImagenBMP *imagen, const char *nombreArchivo);
+//poder llamarla en cualquier momento del programa
 
 
 #endif // BMP_H_INCLUDED
+

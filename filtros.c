95b021d9fd088a9b31e6cba114a6787e0635933b
Anto <130939529+AntonellaGahonaR@users.noreply.github.com>
2026-06-03 00:11:34 -0300

    Refactor filtros.c for filter application and error handling
    Updated the 'bucle_filtros' function to handle filters correctly and added error handling for image processing. Implemented 'aplicar_negativo' and 'aplicar_grises' functions with error checks.

--- filtros.c
+++ filtros.c
@@ -1,9 +1,12 @@
 #include "filtros.h"
 #include "matriz_tda.h"
 
-int bucle_filtros(t_Datos datos, tImagenBMP* imagen1, tImagenBMP* imagen2)
+int bucle_filtros(t_Datos datos, tImagenBMP* imagen1)
 {
     int i;
+    int codError = EXITO;
+
+    char nombreFinal[256] = "GAMMA.bmp";
 
     for(i=0;i<datos.cant_filtros;i++)
     {
@@ -12,17 +15,48 @@ int bucle_filtros(t_Datos datos, tImagenBMP* imagen1, tImagenBMP* imagen2)
             printf("Aplicando filtro %s\n", (datos.filtros_pedidos+i)->nombre);
         }
 
-        if(strcmp((datos.filtros_pedidos+i)->nombre,"--negativo")==0)
-        {
-            //aplicar_filtro1(imagen1, imagen2, (datos.filtros_pedidos+i)->parametro);
+        //FILTRO: NEGATIVO----------------------------------------------------------------------
+        else if(strcmp((datos.filtros_pedidos+i)->nombre,"--negativo")==0)
+        {   puts("AAA");
+            codError = aplicar_negativo(imagen1);
+            if(codError != EXITO){
+                bmp_destruir_imagen(imagen1);
+                return codError;
+            }
+
+            adicionar_filtro(nombreFinal,
+                             (datos.filtros_pedidos+i)->nombre,
+                             (datos.filtros_pedidos+i)->parametro);
+
         }
+
+        //FILTRO: ESCALA DE GRISES ----------------------------------------------------------
         else if(strcmp((datos.filtros_pedidos+i)->nombre,"--escala-de-grises")==0)
         {
-            //aplicar_filtro2(imagen1, imagen2, (datos.filtros_pedidos+i)->parametro);
+            puts("AAA");
+            codError = aplicar_grises(imagen1);
+            if(codError != EXITO){
+                bmp_destruir_imagen(imagen1);
+                return codError;
+            }
+
+            adicionar_filtro(nombreFinal,
+                             (datos.filtros_pedidos+i)->nombre,
+                             (datos.filtros_pedidos+i)->parametro);
         }
-        else if(strcmp((datos.filtros_pedidos+i)->nombre,"--espejar-horizontal")==0)
+        //FILTRO: ESPEJAR HORIZONALMENTE ----------------------------------------------------------
+        else if(strcmp((datos.filtros_pedidos+i)->nombre,"--espejar-horizontal")==0)//revisar funcion
         {
-            //aplicar_filtro3(imagen1, imagen2, (datos.filtros_pedidos+i)->parametro);
+            puts("AAA");
+            //codError =
+            espejar_horizontal(imagen1); //DEBERIA DEVOLVER UN ERROR
+            /*if(codError != EXITO){
+                bmp_destruir_imagen(imagen1);
+                return codError;
+            }*/
+            adicionar_filtro(nombreFinal,
+                            (datos.filtros_pedidos+i)->nombre,
+                            (datos.filtros_pedidos+i)->parametro);
         }
         else if(strcmp((datos.filtros_pedidos+i)->nombre,"--espejar-vertical")==0)
         {
@@ -82,14 +116,38 @@ int bucle_filtros(t_Datos datos, tImagenBMP* imagen1, tImagenBMP* imagen2)
         }
     }
 
-    return EXITO; 
+    codError = bmp_escribir_imagen(nombreFinal, imagen1);
+    if(codError != EXITO)
+    {
+        bmp_destruir_imagen(imagen1);
+        return codError;
+    }
+
+
+    return EXITO;
 }
 
+void adicionar_filtro(char *nombreArchivo, const char *nombreFiltro, int parametro)
+{
+    strrchr(nombreArchivo, '.')[0] = '\0';//corto antes del .bmp
 
-void aplicar_negativo (tImagenBMP* imagen)
+    strcat(nombreArchivo, "_");
+    strcat(nombreArchivo, nombreFiltro+2); //asi no me salen los --
+
+    //agrego el parametro si existe
+    if(parametro != 0){
+        char param[16];
+        sprintf(param, "[%d]", parametro);
+        strcat(nombreArchivo, param);
+    }
+    strcat(nombreArchivo,"_imagen.bmp");
+}
+
+int aplicar_negativo (tImagenBMP* imagen)
 {
     int alto =imagen->cabecera.info.altoImagen;
     int ancho =imagen->cabecera.info.anchoImagen;
+    int codigo = EXITO;
 
     tPixelBMP pixel;
     int i,j;
@@ -98,23 +156,32 @@ void aplicar_negativo (tImagenBMP* imagen)
     {
         for(j=0;j<ancho;j++) //j=columnas
         {
-            matriz_get(&imagen->pixeles,i,j,&pixel); //trae los pixeles
-
+            codigo = matriz_get(&imagen->pixeles,i,j,&pixel); //trae los pixeles
+            if(codigo != EXITO){
+                bmp_destruir_imagen(imagen);
+                return codigo;
+            }
             pixel.azul = 255- pixel.azul;
             pixel.rojo = 255- pixel.rojo;
             pixel.verde = 255- pixel.verde;
 
-            matriz_set(&imagen->pixeles,i,j,&pixel); //cambia los pixeles
+            codigo = matriz_set(&imagen->pixeles,i,j,&pixel); //cambia los pixeles
+            if(codigo != EXITO){
+                bmp_destruir_imagen(imagen);
+                return codigo;
+            }
         }
-    }
+    }//fin FOR
+    return EXITO;
 }
 
-void aplicar_grises (tImagenBMP* imagen)
+int aplicar_grises (tImagenBMP* imagen)
 {
     int alto =imagen->cabecera.info.altoImagen;
     int ancho =imagen->cabecera.info.anchoImagen;
     int suma;
     unsigned char gris;
+    int codigo = EXITO;
 
     tPixelBMP pixel;
     int i,j;
@@ -123,7 +190,11 @@ void aplicar_grises (tImagenBMP* imagen)
     {
         for(j=0;j<ancho;j++) //j=columnas
         {
-            matriz_get(&imagen->pixeles,i,j,&pixel); //trae los pixeles
+            codigo = matriz_get(&imagen->pixeles,i,j,&pixel); //trae los pixeles
+            if(codigo != EXITO){
+                bmp_destruir_imagen(imagen);
+                return codigo;
+            }
 
             suma= (pixel.azul + pixel.verde + pixel.rojo);
             gris=(unsigned char) (suma/3);
@@ -132,13 +203,18 @@ void aplicar_grises (tImagenBMP* imagen)
             pixel.azul = gris;
             pixel.verde = gris;
 
-            matriz_set(&imagen->pixeles,i,j,&pixel); //cambia los pixeles
+            codigo = matriz_set(&imagen->pixeles,i,j,&pixel); //cambia los pixeles
+            if(codigo != EXITO){
+                bmp_destruir_imagen(imagen);
+                return codigo;
+            }
         }
 
     }
+    return EXITO;
 }
 
-void espejar_horizontal (tImagenBMP* imagen)
+void espejar_horizontal (tImagenBMP* imagen) //VER: NO SE ESPEJA, QUEDA IGUAL Y NO APLICA NOMBRE
 {
     int alto =imagen->cabecera.info.altoImagen;
     int ancho =imagen->cabecera.info.anchoImagen;
@@ -199,7 +275,7 @@ void aumentar_contraste (tImagenBMP* imagen, const int parametro)
 /* "--negativo","--escala-de-grises",
     "--espejar-horizontal","--espejar-vertical",     SANTY
     "--aumentar-contraste"
-    
+
     "--reducir-contraste", "--tonalidad-azul",
     "--tonalidad-verde",
     "--tonalidad-roja","--recortar",
@@ -207,4 +283,5 @@ void aumentar_contraste (tImagenBMP* imagen, const int parametro)
     "--achicar","--rotar-derecha",
     "--rotar-izquierda","--concatenar-horizontal",
     "--concatenar-vertical","--comodin"*/
+
 
